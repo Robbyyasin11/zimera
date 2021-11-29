3562f5cea39e9f73260717ce6b1f06f5316048ef
Robbyyasin11 <robbyyasin4@gmail.com>
2021-11-29 19:58:26 +0700

    penambahan folder Zimera

---
+++ novice/01-01/kalkulator.py
@@ -0,0 +1,10 @@
+print(2 + 2)
+print(50 - 5*6)
+print((50 - 5*6) / 4)
+print(8 / 5)  # pembagian
+print(17 / 3) # pembagian
+print(17 // 3)  # pembagian dan hasilnya di tampilkan lengkap
+print(17 % 3)  # sisa bagi %
+print(5 * 3 + 2)  # perkalian dan penjulahan
+print(5 ** 2)  # 5 
+print(2 ** 7)  # 2 pangkat 7
---
+++ novice/README.md
@@ -0,0 +1,9 @@
+
+    Judul: Bahasa Pemrograman Python
+    Oleh: Nur Muhamad Roby Yasin
+    Tanggal: 25-11-21
+    Ringkasan materi : Penenalan Bahas python
+    Penjelasan tentang isi repo
+      - Minggu ke-01
+          - Hari 1: Python, Ekosistem Python, dan Dasar-dasar Pemrograman Python
+    Lisensi
